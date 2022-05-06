b95fee3103b8d33e888de0c6c96fd9937c3e6439
katya <kotenokches@rambler.ru>
2022-05-06 18:04:14 +0500

    ДЗ 17 выполнено - основы алгоритмов 2

---
+++ src/skyro/company/Search.java
@@ -0,0 +1,23 @@
+package skyro.company;
+
+public class Search {
+    public static boolean contains(int[] arr, int element) {
+        int min = 0;
+        int max = arr.length - 1;
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (element == arr[mid]) {
+                return true;
+            }
+
+            if (element < arr[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return false;
+    }
+}
